8ff89d7877d39f90a4d659dd1753352cc03d461a
bhanuprakashsfdc <bhanusunny7@gmail.com>
2024-05-08 09:41:39 +0530

    fixing issue commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://bhanuprakashsfdc.github.io/collegesite/",
+  base: "/",
   plugins: [react()],
 })
